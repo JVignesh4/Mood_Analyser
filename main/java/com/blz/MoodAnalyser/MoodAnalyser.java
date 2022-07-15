2db4c5c1f0504ff47475312805ed2a4fd2f5e45b
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-15 08:56:03 +0530

    Added: Returns Happy while Checking Mood Happy With TestCase

--- main/java/com/blz/MoodAnalyser/MoodAnalyser.java
+++ main/java/com/blz/MoodAnalyser/MoodAnalyser.java
@@ -3,20 +3,16 @@ package com.blz.MoodAnalyser;
 public class MoodAnalyser {
     public static void main(String[] args) {
         System.out.println("Welcome To Mood Analyser");
-
-        //creating a object
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        System.out.println(moodAnalyser.analyseMood("Hi, I Am vignesh , I am sad"));
-
     }
 
-    //UC1
     /*creating a function for checking the mood Happy or Sad
     input we are passing some String message
     output the mood is Happy or sad will get.*/
 
-    public String analyseMood(String message){
-        return "Sad";
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "Sad";
+        else
+            return "Happy";
     }
-
 }
--- test/java/MoodAnalyserTest.java
+++ test/java/MoodAnalyserTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
+
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("hello, how are you?");
-        Assert.assertEquals("Sad",mood);
+        String mood = moodAnalyser.analyseMood("I'm in Any Mood");
+        Assert.assertEquals("Happy",mood);
     }
 
 }
